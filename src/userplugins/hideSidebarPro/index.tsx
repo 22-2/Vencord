b89ce04205e0854b9e255599d165d8e0b163cfe5
22-2 <17890m@protonmail.ch>
2025-12-18 18:35:51 +0900

    fix(hideSidebarPro): Resolve issue where sidebar remained visible

--- src/userplugins/hideSidebarPro/index.tsx
+++ src/userplugins/hideSidebarPro/index.tsx
@@ -17,88 +17,30 @@
 */
 
 import { definePluginSettings } from "@api/Settings";
-import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-import { findComponentByCodeLazy } from "@webpack";
-import { React } from "@webpack/common";
 
 import "./style.css";
 
-const HeaderBarIcon = findComponentByCodeLazy(".HEADER_BAR_BADGE_TOP:", '.iconBadge,"top"');
-
 const settings = definePluginSettings({
-    hideServers: {
-        type: OptionType.BOOLEAN,
-        description: "Reveal Servers on Hover",
-        default: false,
-        onChange: (v) => document.body.classList.toggle("vc-hds-hide-servers", v)
-    },
     hideMembers: {
         type: OptionType.BOOLEAN,
         description: "Reveal Members on Hover",
-        default: false,
-        onChange: (v) => document.body.classList.toggle("vc-hds-hide-members", v)
-    },
-    smallServerList: {
-        type: OptionType.BOOLEAN,
-        description: "Small Server List",
-        default: false,
-        onChange: (v) => document.body.classList.toggle("vc-hds-small-server-list", v)
-    },
-    showButton: {
-        type: OptionType.BOOLEAN,
-        description: "Show Toggle Button in Header",
         default: true,
-        restartNeeded: true
+        onChange: (v) => document.body.classList.toggle("vc-hds-hide-members", v)
     }
 });
 
 export default definePlugin({
     name: "HideSidebarPro",
-    description: "Hides the sidebar and reveals on hover.",
+    description: "Hides the members list and reveals on hover.",
     authors: [{ name: "atetrax", id: 0n }, { name: "JamesN-dev", id: 0n }],
     settings,
 
-    patches: [
-        {
-            find: '?"BACK_FORWARD_NAVIGATION":',
-            replacement: {
-                match: /(?<=trailing:.{0,50})\i\.Fragment,\{(?=.+?className:(\i))/,
-                replace: "$self.TrailingWrapper,{className:$1,"
-            },
-            predicate: () => settings.store.showButton
-        }
-    ],
-
-    TrailingWrapper({ children, className }: { children?: any; className: string; }) {
-        const { hideServers } = settings.use(["hideServers"]);
-
-        return (
-            <>
-                {children}
-                <HeaderBarIcon
-                    className={className}
-                    icon={() => (
-                        <svg width="24" height="24" viewBox="0 0 24 24">
-                            <path fill="currentColor" d="M20 7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7zM6 7h12v2H6V7zm0 4h12v2H6v-2zm0 4h12v2H6v-2z" />
-                            {hideServers && <path fill="currentColor" d="M3.707 2.293a1 1 0 0 0-1.414 1.414l18 18a1 1 0 0 0 1.414-1.414l-18-18z" />}
-                        </svg>
-                    )}
-                    tooltip={hideServers ? "Show Servers" : "Hide Servers"}
-                    onClick={() => settings.store.hideServers = !hideServers}
-                    selected={hideServers}
-                />
-            </>
-        );
-    },
-
     start() {
-        if (settings.store.hideServers) document.body.classList.add("vc-hds-hide-servers");
         if (settings.store.hideMembers) document.body.classList.add("vc-hds-hide-members");
-        if (settings.store.smallServerList) document.body.classList.add("vc-hds-small-server-list");
     },
 
     stop() {
-        document.body.classList.remove("vc-hds-hide-servers", "vc-hds-hide-members", "vc-hds-small-server-list");
+        document.body.classList.remove("vc-hds-hide-members");
     }
 });
